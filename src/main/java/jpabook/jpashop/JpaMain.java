a34eb98124577f27b5a6413956f860b3db19c223
yerin1024 <yerin102@nate.com>
2025-03-06 17:08:15 +0900

    관계형 DB 중심의 엔티티 생성(14~18강)

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); //내부적으로 데이터베이스 커넥션을 가지고 동작하기 때문에 꼭 닫아줘야함
+        }
+
+        emf.close();
+
+    }
+}
